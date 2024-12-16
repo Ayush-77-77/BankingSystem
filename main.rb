f267cc4a2eae8e0a77da83cd376854d7afd78e02
Ayush-77-77 <ayush95758@gmail.com>
2024-12-16 19:34:11 +0530

    implement login method and also fix the bug inside its if function which was getting executed even if the email and password does not match to validate

--- main.rb
+++ main.rb
@@ -1,16 +1,24 @@
 require_relative 'dash_board'
 require_relative 'authentication'
 require_relative 'registration'
+require_relative 'member_details'
 
 class Main
-  bank_employee = Authentication.new
-  bank_employee.user_authentication?("email", "password")
   
-  if (bank_employee)
-   dashboard = DashBoard.new
-   dashboard.dash_service
-  else
-    puts "Incorrect email or password"
+  def login()
+    bank_employee = Authentication.new
+    bank_employee.user_authentication?("email", "password")
+    if (bank_employee==true)
+      dashboard = DashBoard.new
+      dashboard.dash_service
+    else
+      puts "Email or password is wrong"
+    end
   end
+   
+   
     
 end
+
+Main.new.login()  
+  
